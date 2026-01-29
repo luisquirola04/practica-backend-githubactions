a6928953c34378a1e0a1c01553930be469257374
luisquirola04 <132861783+luisquirola04@users.noreply.github.com>
2026-01-29 10:04:06 -0500

    actions actualizacion, llamada desde el main1

--- app.py
+++ app.py
@@ -1,32 +1,34 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 pymysql.install_as_MySQLdb()
+
 from init_base import init
 
 Base = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, instance_relative_config = False)
-
+    app = Flask(__name__, instance_relative_config=False)
     CORS(app)
-    
-    #TODO
+
     app.config.from_object('config.config.Config')
-    
+
     Base.init_app(app)
-    
+
     with app.app_context():
         from routes.route_person import url_person
         from routes.route_product import url_product
-        
+
         app.register_blueprint(url_person)
         app.register_blueprint(url_product)
-        
-        init()
-        # create db tables
-        Base.create_all()
-        #Base.drop_all()
-    
+
+        # 🔥 CLAVE
+        if os.getenv("CI") == "true":
+            print("⚠️ CI detectado: BD deshabilitada")
+        else:
+            init()
+            Base.create_all()
+
     return app
